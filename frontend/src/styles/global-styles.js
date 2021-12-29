00a9999cc769117f068a9ea822a7e199c3c44329
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-29 15:24:26 -0300

    feat(frontend): Add atribute margin using default values by theme in all headings tags

--- frontend/src/styles/global-styles.js
+++ frontend/src/styles/global-styles.js
@@ -17,6 +17,7 @@ export const GlobalStyles = createGlobalStyle`
   }
 
    h1, h2, h3, h4, h5, h6{
-    font-family: ${({ theme }) => theme.font.family.secondary}
+    font-family: ${({ theme }) => theme.font.family.secondary};
+    margin: ${({ theme }) => theme.spacings.large} 0;
   }
 `;
