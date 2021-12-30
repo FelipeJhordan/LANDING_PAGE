261e738273a2ecec50a6f04e56ef685ea40e379c
Felipe Jhordan <felipejordan.alves@gmail.com>
2021-12-30 09:14:40 -0300

    fix(frontend): Fix Component Footer: change importation, change atribute name, update snapshot fix propname in parent component "Base" and update his snapshot

--- frontend/src/components/Footer/Footer.test.jsx
+++ frontend/src/components/Footer/Footer.test.jsx
@@ -1,9 +1,9 @@
 import { Footer } from '.';
-import { renderTheme } from '../styles/render-theme';
+import { renderTheme } from '../../styles/render-theme';
 
 describe('<Footer />', () => {
   it('should render', () => {
-    const { container } = renderTheme(<Footer html={'<h1>Olá</h1>'} />);
+    const { container } = renderTheme(<Footer footerHtml={'<h1>Olá</h1>'} />);
     expect(container).toMatchSnapshot();
   });
 });
